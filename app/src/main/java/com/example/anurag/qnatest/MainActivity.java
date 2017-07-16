74a0ca4eed446ce466959f3f1515e92ad0eb4d0b
anurag <anuragsidana22@gmail.com>
2017-07-16 19:20:31 +0530

    first commit

---
+++ app/src/main/java/com/example/anurag/qnatest/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.anurag.qnatest;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+
+    public class MainActivity extends AppCompatActivity {
+
+        Button mButton;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            mButton = (Button) findViewById(R.id.button);
+            mButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    RetrieveFeedTask task = new RetrieveFeedTask();
+                    task.execute();
+                }
+            });
+        }
+
+
+    // Create GetText Metod
+    public void GetText() throws UnsupportedEncodingException {
+
+        String text = "";
+        BufferedReader reader = null;
+
+        // Send data
+        try {
+
+            // Defined URL  where to send data
+            URL url = new URL("https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/9963aff6-437d-43e2-948f-8b94c9dd30cf/generateAnswer");
+
+            // Send POST data request
+
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+
+            conn.setRequestProperty("Ocp-Apim-Subscription-Key", "");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            //Create JSONObject here
+            JSONObject jsonParam = new JSONObject();
+            jsonParam.put("question", "What is HIV?");
+
+
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+//            wr.write(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+            wr.write(jsonParam.toString());
+            wr.flush();
+            Log.d("karma", "json is " + jsonParam);
+
+            // Get the server response
+
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+
+
+            // Read Server Response
+            while ((line = reader.readLine()) != null) {
+                // Append server response in string
+                sb.append(line + "\n");
+            }
+
+
+            text = sb.toString();
+            Log.d("karma ", "response is " + text);
+        } catch (Exception ex) {
+            Log.d("karma", "exception at last " + ex);
+        } finally {
+            try {
+
+                reader.close();
+            } catch (Exception ex) {
+            }
+        }
+
+
+    }
+
+    class RetrieveFeedTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Log.d("karma", "called");
+                GetText();
+                Log.d("karma", "after called");
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                Log.d("karma", "Exception occurred " + e);
+            }
+
+            return null;
+        }
+    }
+
+
+}
